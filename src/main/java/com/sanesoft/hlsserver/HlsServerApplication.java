9c2484a32c5f27c0a727df38cf2a63f9b8af4a86
kmirocha <kmirocha01@gmail.com>
2020-12-14 12:59:15 +0100

    * inital commit

---
+++ src/main/java/com/sanesoft/hlsserver/HlsServerApplication.java
@@ -0,0 +1,13 @@
+package com.sanesoft.hlsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlsServerApplication.class, args);
+	}
+
+}
